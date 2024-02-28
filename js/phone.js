2676cf628eae62debd1c7f2ee5ba05d3795638b5
anissarkeraraf <anissarkeraraf@gmail.com>
2024-02-28 11:47:34 +0600

    Show All Button To Display All Data

--- js/phone.js
+++ js/phone.js
@@ -1,14 +1,29 @@
-const loadPhone = async (inputFieldTex) => {
-    const res = await fetch (`https://openapi.programming-hero.com/api/phones?search=${inputFieldTex}`);
+const loadPhone = async (inputFieldTex, isShowAll) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${inputFieldTex}`);
     const data = await res.json();
     const phones = data.data;
-    displayPhones(phones)
+    displayPhones(phones, isShowAll)
 }
 
-const displayPhones = phones => {
+const displayPhones = (phones, isShowAll) => {
     const phoneContainer = document.getElementById('phone-container');
-    phoneContainer.textContent =``;
+    phoneContainer.textContent = ``;
     console.log(phones)
+
+    const showAllButton = document.getElementById('show-all-button')
+    if(phones.length > 12 && !isShowAll){
+        showAllButton.classList.remove('hidden')
+    }else{
+        showAllButton.classList.add('hidden')
+    }
+    console.log('is show all button', isShowAll)
+    
+// show 15 phones
+    if(!isShowAll){
+        phones = phones.slice(0, 12);
+    }
+
+
     phones.forEach(phone => {
         console.log(phone)
         const phoneCard = document.createElement('div');
@@ -26,14 +41,39 @@ const displayPhones = phones => {
         `;
         phoneContainer.appendChild(phoneCard);
 
-    })
+    });
+
+    // hide loading section
+    traggleLoading(false)
 }
 
-const searchLoadedPhone = () =>{
+const searchLoadedPhone = (isShowAll) => {
+    traggleLoading(true);
     const inputField = document.getElementById('input-field');
     const inputFieldTex = inputField.value;
     console.log(inputFieldTex)
-    loadPhone(inputFieldTex);
+    loadPhone(inputFieldTex, isShowAll);
+}
+
+// const searchLoadedPhone2 = () => {
+//     traggleLoading(true);
+//     const inputField = document.getElementById('input-field2');
+//     const inputFirldTex = inputField.value;
+//     loadPhone(inputFirldTex);
+// }
+
+const traggleLoading = (isLoading) =>{
+    const loading = document.getElementById('loading');
+    if(isLoading){
+        loading.classList.remove('hidden');
+    }
+    else{
+        loading.classList.add('hidden');
+    }
+}
+
+const showAllButton = () =>{
+    searchLoadedPhone(true);
 }
 
 
